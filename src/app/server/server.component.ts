82d3903688e6b630965831e24ec7c1506c7da181
Gunjan Kadu <agunjan.kadu@gmail.com>
2021-12-06 23:11:35 +0100

    more

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,6 +6,16 @@ import { Component } from "@angular/core";
   styleUrls: ["./server.component.css"]
 })
 export class ServerComponent {
-  public info = "Server Component";
+  info: string = "Server Component 1";
+  id: number = 2;
+  isDisabled: boolean = false;
   constructor() {}
+
+  getInfo() {
+    return this.info;
+  }
+  onServerAdd() {
+    console.log("Server Was Added Successfully");
+    this.isDisabled = true;
+  }
 }
